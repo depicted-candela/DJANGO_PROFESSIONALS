6c79be4c5e51d1ee086407c404719c6485815922
depicted-candela <nicalcoca@gmail.com>
2024-02-29 18:30:31 -0500

    Without many things

---
+++ README.md
@@ -0,0 +1,2 @@
+# DJANGO_PROFESSIONALS
+Here exercises from the book Django for Professionals 4.0
---
+++ ch2-dockerized_hello/pages/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import HttpResponse
+
+def home_page_view(request):
+    return HttpResponse("HOla people")
